9ad9d3302e6e7908ef80d41239e55b356b7e5ee6
joelsomto <somtochukwujoel403@gmail.com>
2025-07-03 16:12:10 +0100

    Db Conns

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '',
-  database: process.env.DB_NAME || 'herald_activate',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
   connectTimeout: 60000
 });
